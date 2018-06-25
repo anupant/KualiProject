0b3235f0f35e3e695a0317f8138d1cadab5b7077
Anu <anupant20@gmail.com>
2018-06-24 17:53:27 -0700

    Making first commit

---
+++ src/Elevator.java
@@ -0,0 +1,43 @@
+
+public class Elevator {
+	
+	private int elevatorId;
+	private int currentFloor;
+	private int maxFloor;
+	
+	public Elevator(int elevatorId,int currentFloor,int maxFloor) {
+		this.elevatorId = elevatorId;
+		this.currentFloor = currentFloor;
+		this.maxFloor = maxFloor;
+	}
+	
+	public void moveUp() {
+		
+	}
+	
+	public void moveDown() {
+		
+		
+	}
+	public void notifyingOpenDoor() {
+		
+	}
+    public void notifyingCloseDoor() {
+		
+	}
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+
+	public void setCurrentFloor(int currentFloor) {
+		this.currentFloor = currentFloor;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
